f732628a7fbcc37196b022061c04573aa9dd531f
erduotong <3135351980@qq.com>
2024-10-01 17:15:27 +0800

    关于&分类

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -4,9 +4,49 @@ export default navbar([
     "/",
 
     {
-        text: "时间线",
+        text: "分类",
         link: "/timeline/",
-        icon: "clock"
-    }
+        icon: "database",
+        children: [
+            {
+                text: "全部",
+                link: "/article/",
+                icon: "file-text"
+            },
+
+            {
+                text: "时间线",
+                link: "/timeline/",
+                icon: "clock"
+            },
+            {
+                text: "标签",
+                link: "/tag/",
+                icon: "tag"
+            },
+            {
+                text: "分类",
+                link: "/category/",
+                icon: "folder"
+            },
+        ]
+    },
+    {
+        text: "关于",
+        link: "/about_site/README.md",
+        icon: "info-circle",
+        children: [
+            {
+                text: "关于本站",
+                link: "/about_site/关于本站.md",
+                icon: "info-circle"
+            },
+            {
+                text: "关于我",
+                link: "/about_site/intro.md",
+                icon: "user"
+            },
+        ]
+    },
 
 ]);
--- src/about_site/README.md
+++ src/about_site/README.md
@@ -1,5 +1,5 @@
 ---
-title: 关于本站
+title: 关于
 index: false
 icon: circle-info
 
