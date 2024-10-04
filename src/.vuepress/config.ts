0792de6b0613a24170fdc48f6191fe0ee31f9593
erduotong <3135351980@qq.com>
2024-10-04 20:15:42 +0800

    anyblock插件

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,29 @@
+// noinspection JSConstantReassignment
+
 import {defineUserConfig} from "vuepress";
 import {getDirname, path} from "vuepress/utils"
 import theme from "./theme.js";
 import link_handler from './plugins/link_handler/link_handler.js'
+import ab_mdit from "any-block-converter-markdown-it" // +
+import jsdom from "jsdom"
+const { JSDOM } = jsdom
+const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`, {
+    url: 'http://localhost/', // @warn 若缺少该行，则在mdit+build环境下，编译报错
+});
+// 一些markdown it插件所需的 以及，我把禁用了给他们
+global.window = dom.window
+global.history = dom.window.history
+global.document = dom.window.document
+global.NodeList = dom.window.NodeList
+global.HTMLElement = dom.window.HTMLElement
+global.HTMLDivElement = dom.window.HTMLDivElement
+global.HTMLPreElement = dom.window.HTMLPreElement
+global.HTMLQuoteElement = dom.window.HTMLQuoteElement
+global.HTMLTableElement = dom.window.HTMLTableElement
+global.HTMLUListElement = dom.window.HTMLUListElement
+global.HTMLScriptElement = dom.window.HTMLScriptElement
+dom.window.scrollTo = ()=>{}
+// ==========================
 const __dirname = getDirname(import.meta.url);
 export default defineUserConfig({
     base: "/",
@@ -22,5 +44,11 @@ export default defineUserConfig({
     ],
     extendsMarkdown: (md) => {
         md.use(link_handler)
-    }
+        md.use(ab_mdit)
+    },
+    markdown: {
+        headers: {
+            level: [1, 2, 3, 4, 5, 6] // 不然的话toc会受限
+        },
+    },
 });
