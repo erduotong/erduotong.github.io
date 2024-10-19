96131e422e310cdea100376f4fd21455677fa07f
erduotong <3135351980@qq.com>
2024-10-19 17:23:02 +0800

    允许h1

---
+++ src/.vuepress/plugins/markdown_it/enable_multi_h1.js
@@ -0,0 +1,20 @@
+/**
+ * @fileOverview 允许多个 h1 标题 通过统一的处理方式
+ * 用的是很暴力的正则，但是管用
+ */
+
+// 逻辑: 在第一个 h1 标题前面加上 ---，这样就可以正常显示了
+export default (md) => {
+    const originalRender = md.render;
+
+    md.render = function (src, env) {
+
+        const regex = /^(# .+)/m;
+
+        const processedSrc = src.replace(regex, (match) => {
+            return `---\n${match}`;
+        });
+
+        return originalRender.call(md, processedSrc, env);
+    };
+};
