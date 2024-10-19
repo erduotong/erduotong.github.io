88959ee5c32e1e61c864466101d1c6276cb29d0f
erduotong <3135351980@qq.com>
2024-10-19 13:18:58 +0800

    update README.md

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,6 @@ import {hopeTheme} from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
-
 export default hopeTheme({
     hostname: "https://erduotong.github.io",
     author: {
