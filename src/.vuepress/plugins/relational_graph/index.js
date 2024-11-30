dfb6b51e9f7adf3cfaa39855fe65e7769b5dd199
erduotong <3135351980@qq.com>
2024-11-30 16:25:30 +0800

    分析双链完成

--- src/.vuepress/plugins/relational_graph/index.js
+++ src/.vuepress/plugins/relational_graph/index.js
@@ -1,10 +1,44 @@
+const bioChainMap = {};
+
+function buildBioChainMap(pages) {
+    // 生成双链
+    for (const page of pages) {
+        if (!page.filePathRelative) {
+            continue;
+        }
+        bioChainMap[page.filePathRelative] = {
+            title: page.title,
+            outlink: [],
+            backlink: [],
+        };
+    }
+    // 第二次遍历，判断出链
+    for (const page of pages) {
+        const links = [];
+        for (const link of page.links) {
+            links.push(decodeURIComponent(link.relative));
+        }
+        links.forEach((link) => {
+            if (bioChainMap[link]) {
+                bioChainMap[link].backlink.push(page.filePathRelative);
+                bioChainMap[page.filePathRelative].outlink.push(link);
+            }
+        });
+    }
+    console.log(bioChainMap);
+}
+
 const relational_graph = () => {
 
     return {
         name: "vuepress-plugin-relational-graph",
-        onInitialized: async(options) =>{
-            // console.log(options);
-        }
+        onPrepared: async (app) => {
+            Object.assign(bioChainMap, {});
+            buildBioChainMap(app.pages);
+        },
+        onGenerated: async (app) => {
+            console.log("on Generated 11111111111111111\non generated!~~~~~~~~~~~");
+        },
     };
 };
 export default relational_graph;
