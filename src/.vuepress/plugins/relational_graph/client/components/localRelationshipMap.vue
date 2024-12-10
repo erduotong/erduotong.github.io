460a53cfb68570fa381d21e11b03862429797cd9
erduotong <3135351980@qq.com>
2024-12-10 20:01:39 +0800

    反链面板

---
+++ src/.vuepress/plugins/relational_graph/client/components/localRelationshipMap.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {usePageData} from "vuepress/client";
+
+const data = usePageData();
+// @ts-ignore
+const map_data = data.value?.bioChainData?.localMap;
+console.log(map_data);
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
