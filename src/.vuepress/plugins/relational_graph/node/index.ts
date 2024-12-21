6cc7cb45bcd635cc1f7566b34c04f3a816d6b90c
erduotong <3135351980@qq.com>
2024-12-21 17:44:28 +0800

    全局关系图谱的写入

--- src/.vuepress/plugins/relational_graph/node/index.ts
+++ src/.vuepress/plugins/relational_graph/node/index.ts
@@ -2,22 +2,26 @@
  * @fileOverview index.ts
  * @author erduotong
  */
-import { getDirname, path } from "vuepress/utils";
-import { bioChainMap, buildBioChainMap } from "./buildMapData.js";
-import type { App } from "vuepress/core";
+import {getDirname, path} from "vuepress/utils";
+import {bioChainMap, buildBioChainMap, writeGlobalGraph} from "./buildMapData.js";
+import type {App} from "vuepress/core";
 
 const __dirname = getDirname(import.meta.url);
 
 const relational_graph = () => {
-  return {
-    name: "vuepress-plugin-relational-graph",
-    onInitialized: (app: App) => {
-      Object.assign(bioChainMap, {});
-      // @ts-ignore
-      buildBioChainMap(app.pages);
-    },
-    clientConfigFile: path.resolve(__dirname, "../client/config.ts"),
-  };
+    return {
+        name: "vuepress-plugin-relational-graph",
+        onInitialized: (app: App) => {
+            Object.assign(bioChainMap, {});
+            // @ts-ignore
+            buildBioChainMap(app.pages);
+        },
+        onGenerated: async (app: App) => {
+            await writeGlobalGraph(app);
+        },
+        clientConfigFile: path.resolve(__dirname, "../client/config.ts"),
+
+    };
 };
 
 export default relational_graph;
