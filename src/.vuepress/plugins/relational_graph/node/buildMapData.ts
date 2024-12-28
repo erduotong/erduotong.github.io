a27cf58d23e75c4b6d0a12eb09ba14c09fda1fae
erduotong <3135351980@qq.com>
2024-12-28 17:06:28 +0800

    update

--- src/.vuepress/plugins/relational_graph/node/buildMapData.ts
+++ src/.vuepress/plugins/relational_graph/node/buildMapData.ts
@@ -199,7 +199,7 @@ export async function writeGlobalGraph(app: App): Promise<void> {
     const globalMap = buildGlobalMap();
     const location = app.dir.dest(graphDataName);
     await fs.ensureDir(path.dirname(location));
-    await fs.writeFile(location, `export default ${JSON.stringify(globalMap)}`, 'utf-8');
+    await fs.writeFile(location,JSON.stringify(globalMap), 'utf-8');
     console.log("GlobalRelationalGraph.json has been written to " + location);
 }
 
@@ -207,6 +207,6 @@ export function writeTempGlobalGraph(app: App) {
     const globalMap = buildGlobalMap();
     const location = app.dir.temp(graphDataName);
     fs.ensureDirSync(path.dirname(location));
-    fs.writeFileSync(location, `export default ${JSON.stringify(globalMap)}`, 'utf-8');
+    fs.writeFileSync(location, JSON.stringify(globalMap), 'utf-8');
 
 }
