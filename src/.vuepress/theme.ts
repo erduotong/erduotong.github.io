2f621915590ff508df1e30a6cc03a29ba42febe7
erduotong <3135351980@qq.com>
2025-06-13 00:07:09 +0800

    feat: fuck off Algolia

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -2,6 +2,7 @@ import {hopeTheme} from "vuepress-theme-hope";
 
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
+import {cut} from "nodejs-jieba";
 
 export default hopeTheme({
     hostname: "https://blog.erduotong.com",
@@ -85,50 +86,13 @@ export default hopeTheme({
             categoryId: "DIC_kwDOM1B4zM4Ci9HB",
         },
 
-        docsearch: {
-            apiKey: "8b80f6438a5fed7cfced1d2ef86922a0",
-            indexName: "erduotong",
-            appId: "L68784ZQB2",
-            placeholder: "搜索文档",
-            translations: {
-                button: {
-                    buttonText: '搜索文档',
-                    buttonAriaLabel: '搜索文档',
-                },
-                modal: {
-                    searchBox: {
-                        resetButtonTitle: '清除查询条件',
-                        resetButtonAriaLabel: '清除查询条件',
-                        cancelButtonText: '取消',
-                        cancelButtonAriaLabel: '取消',
-                    },
-                    startScreen: {
-                        recentSearchesTitle: '搜索历史',
-                        noRecentSearchesText: '没有搜索历史',
-                        saveRecentSearchButtonTitle: '保存至搜索历史',
-                        removeRecentSearchButtonTitle: '从搜索历史中移除',
-                        favoriteSearchesTitle: '收藏',
-                        removeFavoriteSearchButtonTitle: '从收藏中移除',
-                    },
-                    errorScreen: {
-                        titleText: '无法获取结果',
-                        helpText: '你可能需要检查你的网络连接',
-                    },
-                    footer: {
-                        selectText: '选择',
-                        navigateText: '切换',
-                        closeText: '关闭',
-                        searchByText: '搜索提供者',
-                    },
-                    noResultsScreen: {
-                        noResultsText: '无法找到相关结果',
-                        suggestedQueryText: '你可以尝试查询',
-                        reportMissingResultsText: '你认为该查询应该有结果？',
-                        reportMissingResultsLinkText: '点击反馈',
-                    },
-                }
+        slimsearch: {
+            indexContent: true,
+            indexOptions: {
+                // 使用 nodejs-jieba 进行分词
+                tokenize: (text, fieldName) =>
+                    fieldName === 'id' ? [text] : cut(text, true),
             },
-
         },
         icon: {
             assets: "fontawesome-with-brands"
