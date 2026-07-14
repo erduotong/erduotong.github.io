560577fad8d9a241f1054b53316b2858ca352c23
erduotong <erduotong@qq.com>
2026-07-14 13:43:59 +0800

    feat: 为菜单添加左侧logo

---
+++ src/components/MobileMenu.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
+</script>
+
+<template>
+  <Sheet>
+    <SheetTrigger>click!</SheetTrigger>
+    <SheetContent></SheetContent>
+  </Sheet>
+
+</template>
