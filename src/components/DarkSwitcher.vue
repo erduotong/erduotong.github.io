95e71dec2f0eb6ebd652f8fcc655be2891128526
erduotong <erduotong@qq.com>
2026-07-14 15:49:37 +0800

    feat: 添加暗色模式切换及导航数据结构

---
+++ src/components/DarkSwitcher.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {Moon, Sun} from '@lucide/vue'
+import {Button} from "@/components/ui/button";
+import {useDark, useToggle} from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <Button @click="toggleDark()" variant="ghost" size="icon-lg">
+    <Moon v-if="isDark"></Moon>
+    <Sun v-else />
+  </Button>
+</template>
