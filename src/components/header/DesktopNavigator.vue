17a3268c0353ee0d4c804007bed877d07d0ddaf6
erduotong <erduotong@qq.com>
2026-08-25 22:12:18 +0800

    feat: 新增导航菜单组件，集成桌面导航

---
+++ src/components/header/DesktopNavigator.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {
+	NavigationMenu,
+	NavigationMenuLink,
+	navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu'
+</script>
+
+<template>
+  <div class="w-full flex justify-center ">
+    <NavigationMenu :viewport="false">
+      <NavigationMenuLink as-child :class="navigationMenuTriggerStyle()">
+        <a href="/blog/">博客</a>
+      </NavigationMenuLink>
+    </NavigationMenu>
+  </div>
+</template>
