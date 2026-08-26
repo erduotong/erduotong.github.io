5deba4a48363cd1054d6a8173ddad7b9f19f2005
erduotong <erduotong@qq.com>
2026-08-26 19:53:45 +0800

    chore: 修改shadcn-vue style为vega，方便统一圆角样式

--- src/components/ui/sheet/SheetTitle.vue
+++ src/components/ui/sheet/SheetTitle.vue
@@ -15,7 +15,7 @@ const delegatedProps = reactiveOmit(props, 'class')
 <template>
   <DialogTitle
     data-slot="sheet-title"
-    :class="cn('text-foreground text-base font-medium cn-font-heading', props.class)"
+    :class="cn('text-foreground font-medium cn-font-heading', props.class)"
     v-bind="delegatedProps"
   >
     <slot />
